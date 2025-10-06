e3ed0e080620dfff7008441937a22682564b43a9
Ignacio Lopez Torres <ignacio.ujat@gmail.com>
2025-10-05 19:35:32 -0600

    [add]: implementacion de las plantillas de hbs
    implementacion del motor de plantillas hbs

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
  * @requires path
  * @requires cookie-parser
  * @requires morgan
+ * @requires http-errors
  * @requires ./routes/index.routes.js
  * @requires ./routes/users.routes.js
  *
@@ -18,6 +19,7 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from'morgan';
+import createError from 'http-errors';
 
 import indexRouter from './routes/index.routes.js';
 import usersRouter from './routes/users.routes.js';
@@ -25,6 +27,9 @@ import usersRouter from './routes/users.routes.js';
 var app = express();
 
 const __dirname = import.meta.dirname;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -35,5 +40,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
 export default app;
 
